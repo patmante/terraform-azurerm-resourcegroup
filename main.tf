b976b29d296bbff325fab8778397867a512eafea
KalyankarRajesh <46850898+KalyankarRajesh@users.noreply.github.com>
2019-10-23 01:25:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,9 @@ resource "azurerm_resource_group" "res_group" {
     environment = "Test"
   }
 }
+provider "azurerm" {
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
+}
